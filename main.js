739ce5fdfd220a6198eec5fe73507d62bbf7dddb
Rastsislau Krol <avraam87@gmail.com>
2021-10-15 12:26:30 +0300

    cleared logs and comments

--- main.js
+++ main.js
@@ -22,41 +22,25 @@ const createPlayer = (playerClass, playerObj) => {
   const player = document.createElement("div");
   player.classList.add(`${playerClass}`);
   const progressbar = document.createElement("div");
-  const character = document.createElement("div");
-  const life = document.createElement("div");
-  const name = document.createElement("div");
-  const img = document.createElement("img");
-  img.setAttribute("src", `${playerObj.img}`);
-  name.textContent = playerObj.name;
   progressbar.classList.add("progressbar");
+  const character = document.createElement("div");
   character.classList.add("character");
+  const life = document.createElement("div");
   life.classList.add("life");
+  const name = document.createElement("div");
   name.classList.add("name");
+  name.textContent = playerObj.name;
+  const img = document.createElement("img");
   life.setAttribute("style", `width: ${playerObj.hp}%`);
   name.setAttribute("name", playerObj.name);
+  img.setAttribute("src", `${playerObj.img}`);
   player.appendChild(progressbar);
   player.appendChild(character);
   progressbar.appendChild(life);
   progressbar.appendChild(name);
   character.appendChild(img);
   arenas.appendChild(player);
-  console.log("wow");
 };
-
-// const createPlayer = (playerClass) => {
-//   return `
-//     <div class="${playerClass}">
-//       <div class="progressbar">
-//         <div class="life"></div>
-//         <div class="name"></div>
-//       </div>
-//       <div class="character">
-//         <img src="http://reactmarathon-api.herokuapp.com/assets/scorpion.gif" />
-//       </div>
-//     </div>
-//   `;
-// };
-// arenas.insertAdjacentHTML("afterbegin", createPlayer);
 
 createPlayer("player1", player1);
 createPlayer("player2", player2);
