d3640cfc4e2257af101658b5ef9260afd3d0f625
Rastsislau Krol <avraam87@gmail.com>
2021-10-17 15:20:25 +0300

    made a seperate function for random number

--- main.js
+++ main.js
@@ -50,9 +50,11 @@ const createPlayer = (playerObj) => {
   return $player;
 };
 
+const getRandomNumber = () => Math.ceil(Math.random() * 20);
+
 const changeHP = (player) => {
   const $playerLife = document.querySelector(`.player${player.player} .life`);
-  player.hp -= Math.ceil(Math.random() * 20);
+  player.hp -= getRandomNumber();
   if (player.hp < 0) {
     player.hp = 0;
   }
@@ -66,40 +68,40 @@ const gameResult = (name, res) => {
   return $resultTitle;
 };
 
-// const restartGame = (player1, player2) => {
-//   const $player1Life = document.querySelector(`.player1 .life`);
-//   const $player2Life = document.querySelector(`.player2 .life`);
-//   $arenas.removeChild(document.querySelector(".resultTitle"));
-//   player1.hp = 100;
-//   player2.hp = 100;
-//   $player1Life.style.width = player1.hp + "%";
-//   $player2Life.style.width = player2.hp + "%";
-// };
+const restartGame = (player1, player2) => {
+  const $player1Life = document.querySelector(`.player1 .life`);
+  const $player2Life = document.querySelector(`.player2 .life`);
+  $arenas.removeChild(document.querySelector(".resultTitle"));
+  player1.hp = 100;
+  player2.hp = 100;
+  $player1Life.style.width = player1.hp + "%";
+  $player2Life.style.width = player2.hp + "%";
+};
 
 $randomButton.addEventListener("click", () => {
-  // if ($randomButton.textContent === "New Game") {
-  //   restartGame($player1, $player2);
-  //   $randomButton.textContent = "Random";
-  // } else {
-  changeHP($player1);
-  changeHP($player2);
-  if ($player1.hp === 0 && $player2.hp === 0) {
-    $arenas.appendChild(gameResult(" ", "Draw"));
-    $randomButton.disabled = true;
-    // $randomButton.textContent = "New Game";
-    // return;
+  if ($randomButton.textContent === "New Game") {
+    restartGame($player1, $player2);
+    $randomButton.textContent = "Random";
+  } else {
+    changeHP($player1);
+    changeHP($player2);
+    if ($player1.hp === 0 && $player2.hp === 0) {
+      $arenas.appendChild(gameResult(" ", "Draw"));
+      // $randomButton.disabled = true;
+      $randomButton.textContent = "New Game";
+      return;
+    }
+    if ($player1.hp <= 0) {
+      $arenas.appendChild(gameResult($player2.name, "wins"));
+      // $randomButton.disabled = true;
+      $randomButton.textContent = "New Game";
+    }
+    if ($player2.hp <= 0) {
+      $arenas.appendChild(gameResult($player1.name, "wins"));
+      // $randomButton.disabled = true;
+      $randomButton.textContent = "New Game";
+    }
   }
-  if ($player1.hp <= 0) {
-    $arenas.appendChild(gameResult($player2.name, "wins"));
-    $randomButton.disabled = true;
-    // $randomButton.textContent = "New Game";
-  }
-  if ($player2.hp <= 0) {
-    $arenas.appendChild(gameResult($player1.name, "wins"));
-    $randomButton.disabled = true;
-    // $randomButton.textContent = "New Game";
-  }
-  // }
 });
 
 $arenas.appendChild(createPlayer($player1));
